45ae4a5fa2039af878a9897ffab16499c95ab63c
Rishi <rishikeshnepalensis@gmail.com>
2024-11-01 16:41:37 -0400

    data: change blog

--- data/blogsData.js
+++ data/blogsData.js
@@ -1,14 +1,14 @@
 export const BLOGS = [
     {
-    title: "Data Analysis", 
-    description: "Interactive dashboard for visualizing complex datasets", 
-    image: "https://media.licdn.com/dms/image/v2/D4E16AQH5SONW8YxbIw/profile-displaybackgroundimage-shrink_350_1400/profile-displaybackgroundimage-shrink_350_1400/0/1694881293694?e=1735776000&v=beta&t=f1SfEueCH7nK-fpyuOLDkyL8eo4imGUHBd-dLFtZtLQ",
-    link: "https://google.com", 
+        title: "Data Analysis Blog", 
+        description: "Interactive dashboard for visualizing complex datasets", 
+        image: "https://media.licdn.com/dms/image/v2/D4E16AQH5SONW8YxbIw/profile-displaybackgroundimage-shrink_350_1400/profile-displaybackgroundimage-shrink_350_1400/0/1694881293694?e=1735776000&v=beta&t=f1SfEueCH7nK-fpyuOLDkyL8eo4imGUHBd-dLFtZtLQ",
+        link: "https://google.com", 
     },
     {
         title: "Blogs Analysis", 
         description: "Interactive dashboard for visualizing complex datasets", 
         image: "https://media.licdn.com/dms/image/v2/D4E16AQH5SONW8YxbIw/profile-displaybackgroundimage-shrink_350_1400/profile-displaybackgroundimage-shrink_350_1400/0/1694881293694?e=1735776000&v=beta&t=f1SfEueCH7nK-fpyuOLDkyL8eo4imGUHBd-dLFtZtLQ",
         link: "https://google.com", 
-        },
+    },
 ];

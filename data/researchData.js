bee0a1a77e2aedc0a963e756b370980272e71c5c
Rishi <rishikeshnepalensis@gmail.com>
2024-11-02 17:16:12 -0400

    feat: images

--- data/researchData.js
+++ data/researchData.js
@@ -1,9 +1,21 @@
+import Research1 from "@/app/Pictures/IMG_7571.jpg";
+import Research2 from "@/app/Pictures/IMG_7572.jpg";
+
 export const RESEARCH= [
     { 
         title: "Contributors of negative attitudes towards sleep data in biometric-tracking ring users: A qualitative study", 
         description: "This is my poster presentation on my research at American Public Health Association Annual Conference in Minneapolis, Minnesota",
-        image: "portfolio-project/data/Pictures/IMG_7571.jpg",
-        authors:["Hrishikesh Lamichhane1, Lydia Trainor, PhD2, Barbara Araoz, MS1, Marissa McElrone, PhD, RDN, CPH3 and Chelsea Allison, Ph.D., RDN4 (1)Springfield College, Springfield, MA, (2)Rider University, Lawrenceville, NJ, (3)University of Tennessee at Chattanooga, Chattanooga, TN, (4)Longmeadow, MA"],
+        image: Research1.src,
+        authors:["Hrishikesh Lamichhane1, Lydia Trainor, PhD2, Barbara Araoz, MS1, Marissa McElrone, PhD, RDN, CPH3 and Chelsea Allison, Ph.D., RDN4 \n (1)Springfield College, Springfield, MA, (2)Rider University, Lawrenceville, NJ, (3)University of Tennessee at Chattanooga, Chattanooga, TN, (4)Longmeadow, MA"],
+        link: "https://google.com", 
+        date: "2024-10-27",
+        journal: "American Public Health Journal"
+      },
+      { 
+        title: "Employee definitions of wellness at a higher education institution with a health-focused mission", 
+        description: "I worked on Dr. Allison's research project as her Research Assistant and I am working on the manuscript that is under preperation as a secondary author",
+        image: Research2.src,
+        authors:["Chelsea Allison, Ph.D., RDN1, Lydia Trainor, PhD2, Barbara Araoz, MS1, Hrishikesh Lamichhane1, Marissa McElrone, PhD, RDN, CPH3 and Christine Selby, PhD, CEDS, CMPC-E1"],
         link: "https://google.com", 
         date: "2024-10-27",
         journal: "American Public Health Journal"

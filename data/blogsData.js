59673c11a7da1ef1b57791cd43de746423002a3f
Rishi <rishikeshnepalensis@gmail.com>
2025-01-04 19:05:25 -0500

    changed link

--- data/blogsData.js
+++ data/blogsData.js
@@ -1,9 +1,9 @@
 export const BLOGS = [
     {
-        title: "Data Analysis Blog", 
+        title: "Alaska: A FanFiction", 
         description: "Interactive dashboard for visualizing complex datasets", 
         image: "https://media.licdn.com/dms/image/v2/D4E16AQH5SONW8YxbIw/profile-displaybackgroundimage-shrink_350_1400/profile-displaybackgroundimage-shrink_350_1400/0/1694881293694?e=1735776000&v=beta&t=f1SfEueCH7nK-fpyuOLDkyL8eo4imGUHBd-dLFtZtLQ",
-        link: "https://google.com", 
+        link: "https://www.blogger.com/blog/post/edit/preview/4758537145455001661/4084995983241625541", 
     },
     {
         title: "Blogs Analysis", 

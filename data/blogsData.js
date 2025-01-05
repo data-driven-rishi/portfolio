71bc44cb8f33ed5726323912c3aabb427ebe8b75
Rishi <rishikeshnepalensis@gmail.com>
2025-01-04 19:37:49 -0500

    changed the description for my blogs

--- data/blogsData.js
+++ data/blogsData.js
@@ -1,7 +1,7 @@
 export const BLOGS = [
     {
         title: "Alaska: A FanFiction", 
-        description: "Interactive dashboard for visualizing complex datasets", 
+        description: "This project is shows creative writing skills. Here I have combined three major ideas: John Green's Looking for Alaska, The Adult by Bronwyn Fischer and Taylor Swift songs ", 
         image: "https://media.licdn.com/dms/image/v2/D4E16AQH5SONW8YxbIw/profile-displaybackgroundimage-shrink_350_1400/profile-displaybackgroundimage-shrink_350_1400/0/1694881293694?e=1735776000&v=beta&t=f1SfEueCH7nK-fpyuOLDkyL8eo4imGUHBd-dLFtZtLQ",
         link: "https://www.blogger.com/blog/post/edit/preview/4758537145455001661/4084995983241625541", 
     },

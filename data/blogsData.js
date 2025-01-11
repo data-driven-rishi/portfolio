74046a195cfa81ee7a6c5d38a147b6511a0d0ec8
Rishi <rishikeshnepalensis@gmail.com>
2025-01-11 15:11:02 -0500

    changed the image picture

--- data/blogsData.js
+++ data/blogsData.js
@@ -2,7 +2,7 @@ export const BLOGS = [
     {
         title: "Alaska: A FanFiction", 
         description: "This project is shows creative writing skills. Here I have combined three major ideas: John Green's Looking for Alaska, The Adult by Bronwyn Fischer and Taylor Swift songs ", 
-        image: "https://media.licdn.com/dms/image/v2/D4E16AQH5SONW8YxbIw/profile-displaybackgroundimage-shrink_350_1400/profile-displaybackgroundimage-shrink_350_1400/0/1694881293694?e=1735776000&v=beta&t=f1SfEueCH7nK-fpyuOLDkyL8eo4imGUHBd-dLFtZtLQ",
+        image: "/Users/rishi/Desktop/datadrivenrishi/public/image1.jpeg",
         link: "https://hrishiwrites.blogspot.com/", 
     },
     

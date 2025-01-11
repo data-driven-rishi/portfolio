aaa596980fa0d4eb009cee3b57c9365a8333142a
Rishi <rishikeshnepalensis@gmail.com>
2025-01-11 15:42:36 -0500

    added another section to Blogs

--- data/blogsData.js
+++ data/blogsData.js
@@ -1,4 +1,12 @@
 export const BLOGS = [
+    
+    {
+        title: "Interventional Study", 
+        description: "When I was in 8th Grade in Nepal, I voluntereed in a Research Led by Dr. Rita Thapa and team in collecting data on cardiovascular behavioral risk factors and its prevelance among school going adolescents and designed experiential action tools and integrated them among students for interventional purpose", 
+        image:"https://scontent-bos5-1.xx.fbcdn.net/v/t39.30808-6/464302348_8389711951155591_2062081701599718423_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=833d8c&_nc_ohc=7t-booTue7YQ7kNvgEuuUul&_nc_zt=23&_nc_ht=scontent-bos5-1.xx&_nc_gid=A39p1Vc9zU4KMaZuul0ibzk&oh=00_AYA5ifGhmshfrl3DMMjPMbkectGL7oOol384qmj0XuRBdQ&oe=6788A735",
+        link: "https://globalheartjournal.com/articles/10.5334/gh.818", 
+    },
+    
     {
         title: "Alaska: A FanFiction", 
         description: "This project is shows creative writing skills. Here I have combined three major ideas: John Green's Looking for Alaska, The Adult by Bronwyn Fischer and Taylor Swift songs ", 

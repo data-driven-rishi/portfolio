4bb1e63ed8b29232be4ad491d439f7acee250ec1
Rishi <rishikeshnepalensis@gmail.com>
2025-01-11 15:28:29 -0500

    changed I am Rishi to Data Driven Rishi

--- data/blogsData.js
+++ data/blogsData.js
@@ -2,7 +2,7 @@ export const BLOGS = [
     {
         title: "Alaska: A FanFiction", 
         description: "This project is shows creative writing skills. Here I have combined three major ideas: John Green's Looking for Alaska, The Adult by Bronwyn Fischer and Taylor Swift songs ", 
-        image: "/Users/rishi/Desktop/datadrivenrishi/public/image1.jpeg",
+        image: "/images/image1.jpeg",
         link: "https://hrishiwrites.blogspot.com/", 
     },
     
